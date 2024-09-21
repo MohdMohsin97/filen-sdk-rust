301342d72806c036ead827ae1428b46bfa414c4e
Dorivika <avivek796@gmail.com>
2024-09-21 14:15:59 -0400

    feat(POST) : Auth info API fuly working

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,6 @@ fn main() {
 
     let client = Client::new(String::from("CHIIIT"));
     let req = client
-            .request(method::GET,String::from("/v3/auth/info"),Some(InfoRequest{email: String::from("Rireje3001@heweek.com")})).unwrap();
+            .request(method::POST,String::from("/v3/auth/info"),Some(InfoRequest{email: String::from("Rireje3001@heweek.com")})).unwrap();
     println!("{:#?}",req);   
 }
